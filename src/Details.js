6a68773a4d673e18a617c4855d9f10c9a18384f8
naveenkumar <pandurebelsachin18@gmail.com>
2021-10-19 17:01:41 +0530

    fixed a small bug in previous commit

--- src/Details.js
+++ src/Details.js
@@ -1,17 +1,7 @@
-import Pet from './Pet'
-const Details = ({pet})=>{
+const Details = ()=>{
     return (
         <div>
             <h2>Hi mowa! Welcome to 301 Daries</h2>
-            {/* <Pet 
-                name = {pet.name} 
-                breed={pet.breed} 
-                animal = {pet.animal} 
-                key={pet.id} 
-                images = {pet.images}
-                location={`${pet.city}, ${pet.state}`}
-                id={pet.id}
-            /> */}
         </div>
     );
 }
