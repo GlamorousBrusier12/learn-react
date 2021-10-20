206b3a1523b45906c44590126d5ef200ee47ff77
Naveen <pandurebelsachin18@gmail.com>
2021-10-20 21:34:26 +0530

    made the class component interactive

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component{
 
@@ -20,9 +21,10 @@ class Details extends Component{
         );
     }
     render(){
-        const {animal, name, city, state, breed, description} = this.state;
+        const {animal, name, city, state, breed, description, images} = this.state;
         return (
             <div className = 'details'>
+                <Carousel images={images}/>
                 <div>
                     <h1>{name}</h1>
                     <h2>{`${animal}-${breed}-${city}, ${state}`} </h2>
